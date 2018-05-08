7b4df6b58803e89dd8646306bf5e58f7ca9e472d
Roshan Wagle <roshanwagle@Roshans-MacBook-Pro.local>
2018-05-09 01:24:03 +1000

    Face detection using Local Binary pattern algorithm.

---
+++ face_lbp.py
@@ -0,0 +1,34 @@
+"""open cv face detection implemenation using Local binary Classifer."""
+import cv2
+PATH = 'many-faces.jpg'
+
+
+def grayscale(image_path):
+    """Convert the image to gray color."""
+    MYIMAGE = cv2.imread(image_path)           # read an image
+    return MYIMAGE
+
+
+def load_classifierToDetectFace(gray_image):
+    """Loading a classifier."""
+    face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
+
+    # checking the classifier load is successful or not
+    test = face_cascade.load('lbpcascade_frontalface.xml')
+    if test is True:
+        find_face = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2,
+                                                  minNeighbors=5)
+        print('Faces found: ', len(find_face))
+        if len(find_face) > 0:
+            for (x, y, w, h) in find_face:
+                cv2.rectangle(gray_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            return gray_image
+    return None
+
+
+gray_image = grayscale(PATH)
+image_with_face = load_classifierToDetectFace(gray_image)
+if image_with_face is not None:
+    cv2.imshow('image', image_with_face)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
