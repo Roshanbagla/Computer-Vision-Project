2e9a7b937e8301f4ae9def17df8becce850e1617
Roshan Wagle <roshanwagle@Roshans-MacBook-Pro.local>
2018-05-08 23:50:09 +1000

    Face detection algorithm using cascaade classifier

---
+++ face.py
@@ -0,0 +1,35 @@
+"""open cv face detection implemenation."""
+import cv2
+PATH = 'tony_stark.jpg'
+
+
+def grayscale(image_path):
+    """Convert the image to gray color."""
+    MYIMAGE = cv2.imread(image_path)           # read an image
+    return MYIMAGE
+
+
+def load_classifierToDetectFace(gray_image):
+    """Loading a classifier."""
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # checking the classifier load is successful or not
+    test = face_cascade.load('haarcascade_frontalface_default.xml')
+    if test is True:
+        find_face = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1,
+                                                  minNeighbors=5)
+        print(type(find_face))
+        print('Faces found: ', len(find_face))
+        if len(find_face) > 0:
+            for (x, y, w, h) in find_face:
+                cv2.rectangle(gray_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            return gray_image
+    return None
+
+
+gray_image = grayscale(PATH)
+image_with_face = load_classifierToDetectFace(gray_image)
+if image_with_face is not None:
+    cv2.imshow('image', image_with_face)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
